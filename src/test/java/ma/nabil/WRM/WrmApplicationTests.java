18f09102942d33388a446780fe7465f474835b72
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-11-13 12:01:07 +0100

    Création de structure de projet

---
+++ src/test/java/ma/nabil/WRM/WrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.nabil.WRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
